ccda1b1b4ef17f07d6ac1cb9f0dc583fc7efa229
Psammy <ampadupsamuel1@gmail.com>
2025-05-30 19:18:41 +0000

    complete input

--- src/components/InputPage.tsx
+++ src/components/InputPage.tsx
@@ -13,16 +13,19 @@ const InputPage = () => {
   }
   return (
     <div className="flex items-center justify-center min-h-[100dvh]">
-      <div className="">
+      <div className="h-[30rem]">
         <form className="">
-          <h1 className="text-blue-500 text-2xl">GitHub User Comparison</h1>
+          <h1 className="text-blue-500 text-2xl font-bold mb-20 text-center">GitHub User Comparison</h1>
           <em>Enter the exact github usernames of the two geeks</em>
-        <div className="h-10 w-90 border-2 border-black mb-4 p-2 rounded-lg">
-          <input className="h-full w-full outline-none" name="username1" type="text" placeholder="Enter Github username of first user" value={formData.username1} onChange={handleFormChange}/>
+        <div className="h-10 w-90 border-2 border-black mb-4  rounded-lg">
+          <input className="h-full w-full outline-none p-2" name="username1" type="text" placeholder="Enter Github username of first user" value={formData.username1} onChange={handleFormChange}/>
         </div>
-        <div className="h-10 w-90 border-2 border-black p-2 rounded-lg">
-          <input className="w-full h-full outline-none" type="text" name="username2" placeholder="Enter Github username of second user" value={formData.username2} onChange={handleFormChange} />
+        <div className="h-10 w-90 border-2 border-black rounded-lg">
+          <input className="w-full h-full outline-none p-2" type="text" name="username2" placeholder="Enter Github username of second user" value={formData.username2} onChange={handleFormChange} />
           </div>
+          <div className="w-full text-center">
+            <button className="bg-blue-500 text-white px-10 py-2 rounded-full mt-4">Compare</button>
+</div>
           </form>
       </div>
       
