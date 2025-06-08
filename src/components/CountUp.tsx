248874372d3a11b9b557c39a844de4caf1d885e2
Psammy <ampadupsamuel1@gmail.com>
2025-06-08 15:42:50 +0000

    ui components

---
+++ src/components/CountUp.tsx
@@ -0,0 +1,18 @@
+// import { useEffect, useState } from "react"
+
+// const CountUp = () => {
+//   const [count, setCount] = useState<number>(0);
+
+//   useEffect(() => {
+//     let start = 0;
+//     const duration = 2000;
+//     const interval= Math.abs()
+
+//     setTimeout((),)
+//   },[])
+//   return (
+//  <div></div>
+//   )
+// }
+
+// export default CountUp
